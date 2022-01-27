3103b6920049fdb14e6929d5d5e28fe3a1a58a66
andregoetz <andregoetz37@gmail.com>
2022-01-27 17:11:14 +0100

    better example for return value with std::function and function_ptr

--- src/function_ptr/function_ptr_example.cpp
+++ src/function_ptr/function_ptr_example.cpp
@@ -1,25 +1,30 @@
 #include <iostream>
+#include <sstream>
 #include <functional>
 
 using namespace std;
 
 //std::function_ptr as parameter
-double eval_std_fun(const function<double(double)> &f, double x) {
+string double_to_string_with_std_fun(const function<string(double)> &f, double x) {
     return f(x);
 }
 
-//function_ptr pointer as parameter
-double eval_fun_ptr(double (*f)(double), double x) {
+//function pointer as parameter
+string double_to_string_with_fun_ptr(string (*f)(double), double x) {
     return f(x);
 }
 
 int main() {
-    auto xPow2 = [](double x) -> auto { return x * x; };
-    cout << eval_std_fun(xPow2, 1) << endl;
-    cout << eval_fun_ptr(xPow2, 1) << endl;
-    cout << eval_std_fun(xPow2, 2) << endl;
-    cout << eval_fun_ptr(xPow2, 2) << endl;
-    cout << eval_std_fun(xPow2, 3) << endl;
-    cout << eval_fun_ptr(xPow2, 3) << endl;
+    auto xPow2 = [](double x) -> auto {
+        std::stringstream sstr;
+        sstr << x << " als string";
+        return sstr.str();
+    };
+    cout << double_to_string_with_std_fun(xPow2, 1) << endl;
+    cout << double_to_string_with_fun_ptr(xPow2, 1) << endl;
+    cout << double_to_string_with_std_fun(xPow2, 2) << endl;
+    cout << double_to_string_with_fun_ptr(xPow2, 2) << endl;
+    cout << double_to_string_with_std_fun(xPow2, 3) << endl;
+    cout << double_to_string_with_fun_ptr(xPow2, 3) << endl;
     return 0;
 }
